a89252a308cd37d15103f9c66d00a0680923610b
Bismaon <64341829+Bismaon@users.noreply.github.com>
2023-01-18 11:32:23 -0400

    added differerent settings

---
+++ assets/scripts/jquery.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $(".toggler").click(function () {
+        $("#customSettings").hide();
+    });
+});
+
+$(document).ready(function() {
+    $("#custom").click(function(){
+        $("#customSettings").show();
+    });
+});
